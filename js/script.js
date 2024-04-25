11d77ea2d45b507d7950009b6f018e7fd54b6833
Muthu kumar <muthukumar24081999@gmail.com>
2024-04-25 15:48:42 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 // Function to fetch a new fox image
 function fetchNewFoxImage() 
-{
+{   //Getting the datas using fetch method
     fetch('https://randomfox.ca/floof/')
+        //URL Response handler
         .then((response) => {            
             return response.json();
         })
-        .then((data) => {
+        .then((data) => { //response data handler
             // Update webpage content with the fetched image
             const foxContainer = document.getElementById('fox-container');
             foxContainer.innerHTML = `<img class="fox-image" src="${data.image}" alt="Random Fox">`;
         })
-        .catch((error) => {
+        .catch((error) => { //response data - error handler
             const foxContainer = document.getElementById('fox-container');
             foxContainer.innerHTML = `There was a problem fetching the fox image: ${error}`;
             console.error('There was a problem fetching the fox image:', error);
